863e777e71e07bc671a18b47c868427be1a8795c
Samuel Soyebo <ayodejisoyebo@gmail.com>
2023-04-26 08:40:31 +0100

    Bug fix Page routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Dashboard from "../src/pages/Dashboard";
-import Item1 from "../src/pages/item1";
-import Item2 from "../src/pages/item2";
-import Item3 from "../src/pages/item3";
-import Item4 from "../src/pages/item4";
-import Item5 from "../src/pages/item5";
-import Item6 from "../src/pages/item6";
-import Item7 from "../src/pages/item7";
-import Item8 from "../src/pages/item8";
+import Item1 from "../src/pages/Item1";
+import Item2 from "../src/pages/Item2";
+import Item3 from "../src/pages/Item3";
+import Item4 from "../src/pages/Item4";
+import Item5 from "../src/pages/Item5";
+import Item6 from "../src/pages/Item6";
+import Item7 from "../src/pages/Item7";
+import Item8 from "../src/pages/Item8";
 
 function App() {
   return (
@@ -22,7 +22,7 @@ function App() {
         <Route path="/item6" element={<Item6 />} />
         <Route path="/item7" element={<Item7 />} />
         <Route path="/item8" element={<Item8 />} />
-        <Route path="/notFound" element={<h1>404</h1>} />
+        <Route path="*/notFound" element={<h1>404</h1>} />
       </Routes>
     </Router>
   );
